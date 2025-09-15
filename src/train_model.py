d5ec892bf7e8a286d2940e445052b841c8a60ef7
Pierre SCHWEITZER <pierre.schweitzer@epita.fr>
2025-09-15 15:27:18 +0200

    feat: train model and save it in MLflow (Part1)

---
+++ src/train_model.py
@@ -0,0 +1,49 @@
+import mlflow
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+mlflow.set_tracking_uri("http://localhost:8080")
+
+gdp_experiment = mlflow.set_experiment("GDP_Prediction")
+run_name = "LogisticRegression_GDP_Model"
+artifact_path = "lr_gdp_model"
+
+df = pd.read_csv("../data/gdp_per_country.csv")
+df = df.dropna()
+df_inputs = df[["2020", "2021", "2022", "2023", "2024"]].apply(pd.to_numeric, errors='coerce').dropna()
+df_target = df["2025"].apply(pd.to_numeric, errors='coerce').dropna()
+
+params = {
+    "penalty": "l2",
+    "C": 1.0,
+    "solver": "lbfgs",
+    "max_iter": 1000,
+    "random_state": 42
+}
+
+X_train, X_test, y_train, y_test = train_test_split(df_inputs, df_target, test_size=0.2, random_state=42)
+
+model = LogisticRegression(**params)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+
+mae = mean_absolute_error(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y_test, y_pred)
+
+metrics = {"mae": mae, "mse": mse, "rmse": rmse, "r2": r2}
+
+with mlflow.start_run(run_name=run_name) as run:
+    mlflow.log_params(model.get_params())
+    mlflow.log_metrics(metrics)
+    mlflow.sklearn.log_model(
+        sk_model=model, 
+        input_example=X_test.iloc[:5], 
+        registered_model_name=artifact_path,
+        name=artifact_path
+        )
